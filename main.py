b4c9324f1c2cbb0881c36130b341094830fbe604
Outtech105k <techno510tk@gmail.com>
2025-06-25 12:17:17 +0900

    Impremented Test works
    - main動作を一旦定義

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
-import time
 import lcd
+import motion
+import buzzer
 
 
 def main():
+    motion_sensor = motion.MotionSensor(24)
+    lcd16x2 = lcd.Lcd(0x27)
+    buzz = buzzer.Buzzer(7)
+
     # モーション反応待ち
+    seq_count = 0
+    while True:
+        if motion_sensor.get_is_moved():
+            seq_count += 1
+            if seq_count >= 10:
+                break
+        else:
+            seq_count = 0
+
     # Webカメラ撮影
+    # TODO: Implement here
+    lcd16x2.print_waiting()
+    for i in range(5):
+        buzz.sound_wait()
+    is_detect_approved = True
+
     # 一致で分岐
-    # LCD
-    # Buzzer
-    # API Request
+    if is_detect_approved:
+        lcd16x2.print_approved()
+        buzz.sound_accept()
+        # TODO: API request
+    else:
+        lcd16x2.print_rejected()
+        buzz.sound_reject()
+        # TODO: API request
 
-    # This is test.
-    indicator = lcd.Lcd(0x27)
-    indicator.print_detecting()
-    time.sleep(2)
-    indicator.print_approved()
-    time.sleep(2)
-    indicator.print_rejected()
-    time.sleep(2)
-    indicator.print_error()
-    time.sleep(2)
-
-    try:
-        indicator.print_waiting()
-        time.sleep(0.2)
-    except KeyboardInterrupt:
-        pass
+    motion_sensor.close()
 
 
 if __name__ == '__main__':
